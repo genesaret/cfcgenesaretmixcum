66dd2c7041751060f068df399d81cfb65cca11f8
adalberto1504 <adal_scooter@hotmail.com>
2016-07-16 08:22:54 -0500

    ssssa

--- src/main/java/genesaret/genemixcontacto/LlenarListaContactos.java
+++ src/main/java/genesaret/genemixcontacto/LlenarListaContactos.java
@@ -8,15 +8,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import genesaret.clasesEntidad.Gmcontacto;
-@ManagedBean(name = "LlenarListaContactos")
-@ApplicationScoped
+
 public class LlenarListaContactos {
-	 @PersistenceContext(unitName = "genesaret")
-	    private EntityManager em;
-	     
-	      public List<Gmcontacto> ListaContactos(){
-	        List<Gmcontacto> contactos = new ArrayList<Gmcontacto>(); //inicializamos la lista de objetos de nuestra clase productos 
-	        contactos = em.createQuery("SELECT s FROM Gmcontacto s").getResultList(); //llenamos la lista d eobjetos
-	        return contactos;
-	    }
 }
