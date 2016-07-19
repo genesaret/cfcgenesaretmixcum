c9f8889543e119e9978dfc66f0725c8801c03ae5
adalberto1504 <adal_scooter@hotmail.com>
2016-07-19 05:07:02 -0500

    dsdsdsds

--- src/main/java/genesaret/genemixboletin/GenemixBoletinControlador.java
+++ src/main/java/genesaret/genemixboletin/GenemixBoletinControlador.java
@@ -12,7 +12,9 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
 import javax.transaction.NotSupportedException;
@@ -48,7 +50,41 @@ public Gmboletin getGmBoletin() {
 		this.gmBoletin = gmBoletin;
 	}
 public void addEmail(){
+	try{
+		utx.begin();
+		gmBoletin.setFecha(fecha());
+		
+		try{
+	        Query q1 =em.createQuery("SELECT s.idBoletin FROM Gmboletin s WHERE s.email = :email");
+	        q1.setParameter("email", gmBoletin.getEmail());
+	        if(q1.getSingleResult() == null){
+	            System.out.println("Es nulo");
+	        }
+	        
+	        //mensaje de no creacion
+	           FacesMessage msg = new FacesMessage("oops! Este Email Ya Existe ");
+	           FacesContext.getCurrentInstance().addMessage(null, msg);
+	        
+	    }catch(NoResultException e){
+	            em.persist(gmBoletin);
+	            utx.commit();
+	            FacesMessage msg = new FacesMessage("Email  Enviado con Exito! ", "Email: " + gmBoletin.getEmail());
+	            FacesContext.getCurrentInstance().addMessage(null, msg);
+
+	        
+	    }
+		
+		
+	}
+	catch (Exception e) {
+        FacesMessage msg = new FacesMessage("HA OCURRIDO UN ERROR INESPERADO.. VUELVA A INTENTAR ", "");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        System.out.println(e);
+    }
+	
         
+	
+	/*
         try {
         
             utx.begin();
@@ -67,6 +103,7 @@ public void addEmail(){
             Logger.getLogger(GenemixBoletinControlador.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Error al enviar email  :" +ex.getMessage());
         } 
+      */  
     }
     public Date fecha()
     {
