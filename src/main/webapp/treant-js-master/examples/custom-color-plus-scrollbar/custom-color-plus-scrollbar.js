321e206d4e99ab10a3f140880f27f74d7d470be5
adalberto1504 <adal_scooter@hotmail.com>
2016-11-04 23:34:36 -0600

    HGHJGJ

--- src/main/webapp/treant-js-master/examples/custom-color-plus-scrollbar/custom-color-plus-scrollbar.js
+++ src/main/webapp/treant-js-master/examples/custom-color-plus-scrollbar/custom-color-plus-scrollbar.js
@@ -1,6 +1,6 @@
 var config = {
         container: "#OrganiseChart1",
-        rootOrientation:  'SOUTH', // NORTH || EAST || WEST || SOUTH
+        rootOrientation:  'NORTH', // NORTH || EAST || WEST || SOUTH
         // levelSeparation: 30,
         siblingSeparation:   20,
         subTeeSeparation:    60,
