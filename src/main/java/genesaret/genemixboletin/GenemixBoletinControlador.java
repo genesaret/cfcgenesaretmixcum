c04aa76e2121794b6e772c62a4023708b48821f4
adalberto1504 <adal_scooter@hotmail.com>
2017-06-11 02:47:44 -0500

    tissues

--- src/main/java/genesaret/genemixboletin/GenemixBoletinControlador.java
+++ src/main/java/genesaret/genemixboletin/GenemixBoletinControlador.java
@@ -20,9 +20,9 @@ import genesaret.clasesEntidad.Gmboletin;
  *File: GenemixBoletinControlador.java
  *This class takes care on new user subscriptions 
  */
-//ManagedBean
+@ManagedBean
 @RequestScoped
-@ViewScoped
+//ViewScoped
 public class GenemixBoletinControlador implements Serializable
 {
 	private static final long serialVersionUID = 1L;
