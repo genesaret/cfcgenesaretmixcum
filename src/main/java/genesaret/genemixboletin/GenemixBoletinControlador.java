5b96403bbefdc95c1a0ab46c2077958ea99d0074
adalberto1504 <adal_scooter@hotmail.com>
2017-06-11 13:22:17 -0500

    fixit:  issues with user transaction

--- src/main/java/genesaret/genemixboletin/GenemixBoletinControlador.java
+++ src/main/java/genesaret/genemixboletin/GenemixBoletinControlador.java
@@ -2,6 +2,8 @@ package genesaret.genemixboletin;
 
 import java.io.Serializable;
 import java.sql.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.annotation.Resource;
 import javax.faces.application.FacesMessage;
@@ -13,7 +15,14 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+
 import genesaret.clasesEntidad.Gmboletin;
+import genesaret.genemixcontacto.GenemixContactoControlador;
 /**
  * @author scooter
  *
@@ -53,35 +62,38 @@ public class GenemixBoletinControlador implements Serializable
 		try
 		{
 			utx.begin();
+			gmBoletin.setFecha(fecha());
 			try
 			{
 				//verify whether the user mail is already logged in DB
 				Query q1 =em.createQuery("SELECT s.idBoletin FROM Gmboletin s WHERE s.email = :email");
 				q1.setParameter("email", gmBoletin.getEmail());
+				if(q1.getSingleResult() == null)
+				{
+					System.out.println("Es nulo");
+				}
 				//rollback the transaction whether the query returns null 
 				//print the error message and set the gmBoletin object to NULL
-				FacesMessage msg = new FacesMessage("oops! Este Email Ya Existe ","Try Again");
+				FacesMessage msg = new FacesMessage("Ops! Este Email Ya Existe ","Try Again");
 				FacesContext.getCurrentInstance().addMessage(null, msg);
-				em.getEntityManagerFactory().getCache().evictAll();
-				//utx.rollback();
+				utx.rollback();
+				gmBoletin = null;
 			}
 			catch(NoResultException e)//if not result was triggered in try block we can add the mail to te DB 
 			{
 				//persist the gmBoletin object, commit the transaction and show the successful message
-				gmBoletin.setFecha(fecha());
 				em.persist(gmBoletin);
-				em.getEntityManagerFactory().getCache().evictAll();
+				//em.getEntityManagerFactory().getCache().evictAll();
 				utx.commit();
 				FacesMessage msg = new FacesMessage("Email  Enviado con Exito! ", gmBoletin.getEmail());
 				FacesContext.getCurrentInstance().addMessage(null, msg);
-				//em.close();
 			}
-			gmBoletin=null;	
 		}
-		catch (Exception e) 
+		catch (IllegalStateException | SecurityException | HeuristicMixedException | HeuristicRollbackException | NotSupportedException | RollbackException | SystemException ex) 
 		{
-			FacesMessage msg = new FacesMessage("HA OCURRIDO UN ERROR INESPERADO.. VUELVA A INTENTAR ", "");
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN,  "HA OCURRIDO UN ERROR INESPERADO.. VUELVA A INTENTAR ", "");
 			FacesContext.getCurrentInstance().addMessage(null, msg);
+			Logger.getLogger(GenemixContactoControlador.class.getName()).log(Level.SEVERE, null, ex.getMessage());
 		}
 	}
 	
